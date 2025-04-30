a38e9d91a31c736fb019374ecbc60ad569e379a2
KasuganoSora1 <zhang192831@gmail.com>
2025-04-30 11:16:43 +0800

    新算法已确定，更新算法

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,2 @@
 import "../src/artifact/artifact"
 import { artifact } from "../src/artifact/artifact"
-let art=new artifact(5,15,"xxx","flower",
-    null,
-    null,
-    "head"
-);
-art.getperhasps_EX();
